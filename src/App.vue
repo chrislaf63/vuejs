b7abf8593b50d26be614199ba84b3e8210c1c27a
Christophe LAFARGE <lafarge.christophe@etik.com>
2024-04-26 14:34:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld
+  @updateName ="name => display(name)"
+  />
+
+</template>
+
+<script setup>
+      import HelloWorld from '@/components/HelloWorld.vue';
+
+      const display = (name) => {
+        console.log('App.vue : ' + name);
+      }
+</script>
+
+<style scoped>
+
+</style>
